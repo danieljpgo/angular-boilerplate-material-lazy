060f5957b4f5e1b2068c587f34c771be500976ec
Daniel Jorge <djpgonzaga@gmail.com>
2019-07-31 14:58:53 -0300

    saving code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// Shared Module
+import { SharedModule } from './shared/shared.module';
+
 // Containers
 import { AppComponent } from './app.component';
 
@@ -13,7 +16,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    // @TODO Shared Module
+    SharedModule
   ],
   providers: [],
   bootstrap: [
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+// Material
+import { MatButtonModule } from '@angular/material';
+
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    // Material
+    MatButtonModule
   ],
   exports: [
-    CommonModule
+    CommonModule,
+    // Material
+    MatButtonModule
   ]
 })
 export class SharedModule { }
